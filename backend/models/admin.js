6c6fce81f329e1657516b67155d8cfd7ec012ae7
WaliMuhammadAhmad <2021se39@student.uet.edu.pk>
2024-06-01 06:26:43 +0500

    User and Admin Schema updated, bcrypt used for password encryption

--- backend/models/admin.js
+++ backend/models/admin.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const AdminSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    superAdmin: {type: Boolean}
+    role: { type: String, enum: ['admin', 'SuperAdmin'], default: 'admin' },
 },
 {
     timestamps: true,
 });
+
+UserSchema.pre('save', async function(next) {
+    if (this.isModified('password') || this.isNew) {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+  });
 
 module.exports = mongoose.model('Admin', AdminSchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  pic: {type: String}
-},
-{
+  phone: { type: String, unique: true },
+  address: { type: String, trim: true, lowercase: true },
+  dob: { type: Date },
+  pic: { type: String },
+  payment: { type: String, default: 'cash' },
+  cardNumber: { type: Number, unique: true },
+  cardName: { type: String },
+  cvv: { type: Number, min: 100, max: 999},
+  status: { type: String, enum: ['active', 'inactive', 'banned'], default: 'active' },
+}, {
   timestamps: true,
-}
-);
+});
+
+UserSchema.pre('save', async function(next) {
+  if (this.isModified('password') || this.isNew) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
 
 module.exports = mongoose.model('User', UserSchema);
