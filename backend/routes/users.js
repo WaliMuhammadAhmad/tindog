aa01d15713878c3659f7d2284d78da81f53cd4f1
WaliMuhammadAhmad <2021se39@student.uet.edu.pk>
2024-06-02 20:24:11 +0500

    user routes created

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// GET request for list of all users
-router.get('/', userController.user_list);
+// Route to get all users
+router.get('/', userController.getUsers);
 
-// POST request for creating a user
-router.post('/', userController.user_create_post);
+// Route to get a user by ID
+router.get('/:id', userController.getUserById);
+
+// Route to create a new user
+router.post('/', userController.createUser);
+
+// Route to update a user by ID
+router.put('/:id', userController.updateUser);
+
+// Route to delete a user by ID
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
