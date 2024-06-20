a7f8114180d18314240527bcf4cd3d8f75447dd0
WaliMuhammadAhmad <2021se39@student.uet.edu.pk>
2024-06-20 20:57:49 +0500

    Sign In logic...

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Button } from "react-bootstrap";
 import Topbar from "../components/Navbar";
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000';
 
 function SignIn() {
+
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const emailRegex = /^[a-zA-Z][\w.-]*@[a-zA-Z]+\.[a-zA-Z]{2,3}$/;
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!emailRegex.test(formData.email)) {
+      alert("Invalid email address");
+      return;
+    }
+
+    if (formData.password.length < 6) {
+      alert("Password must be at least 6 characters long");
+      return;
+    }
+
+  };
+
   return (
     <div
       style={{
@@ -22,29 +50,40 @@ function SignIn() {
             padding: "2.5em",
           }}
         >
-          <form>
-            <header className="display-3">SIGN IN</header>
+          <form method="POST" onSubmit={handleSubmit}>
+            <header className="display-3" alignItems="center">SIGN IN</header>
             <div className="mb-3">
-              <label for="email" className="form-label">
+              <label htmlFor="email" className="form-label">
                 Email address
               </label>
               <input
                 type="email"
                 className="form-control"
                 id="email"
-                aria-describedby="emailHelp"
+                value={formData.email}
+                onChange={handleChange}
               />
+              
             </div>
             <div className="mb-3">
-              <label for="password" className="form-label">
+              <label htmlFor="password" className="form-label">
                 Password
               </label>
-              <input type="password" className="form-control" id="password" />
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+              
             </div>
             <button type="submit" className="btn btn-danger">
               Submit
             </button>
-            <Button href="/signup">New to tindog? Sign up</Button>
+            <Button href="/signup" variant="btn" className="ms-3">
+              New to tindog? Sign up
+            </Button>
           </form>
         </div>
       </Container>
